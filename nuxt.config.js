1661dca08b0c4c7e3656d57edff0099a3bdf52de
Tovi Levis <tovi@scrtlabs.com>
2023-02-22 18:04:19 +0200

    Remove unused axios dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,16 +41,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     '@nuxtjs/dotenv',   
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
